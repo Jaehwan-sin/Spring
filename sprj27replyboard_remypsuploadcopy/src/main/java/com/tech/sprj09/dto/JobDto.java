adc9a3b590042133ce26bf760a96cdea93b42958
Jae hwan Shin <84274952+Jaehwan-sin@users.noreply.github.com>
2023-08-09 20:51:09 +0900

    Add files via upload

---
+++ sprj27replyboard_remypsuploadcopy/src/main/java/com/tech/sprj09/dto/JobDto.java
@@ -0,0 +1,17 @@
+package com.tech.sprj09.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobDto {
+	
+	private String job;
+	private int salsum;
+	
+}
